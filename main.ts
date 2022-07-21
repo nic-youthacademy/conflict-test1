2eda094858c5c051f91d662a6cb45bc240901fd8
nic-youthacademy <109627731+nic-youthacademy@users.noreply.github.com>
2022-07-21 15:43:26 -0700

    test2 changes

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-tiles.setCurrentTilemap(tilemap`level3`)
+tiles.setCurrentTilemap(tilemap`level2`)
 let mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . 2 2 . . . . . . . . . 
@@ -17,8 +17,23 @@ let mySprite = sprites.create(img`
     . . . . . . 2 2 2 2 2 . . 2 2 . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
-let mySprite = sprites.create(assets.image`myImage`, SpriteKind.Player)
-controller.moveSprite(mySprite)
+scene.setBackgroundColor(9)
+let mySprite2 = sprites.create(img`
+    . . . . . . b b b b . . . . . . 
+    . . . . . . b 4 4 4 b . . . . . 
+    . . . . . . b b 4 4 4 b . . . . 
+    . . . . . b 4 b b b 4 4 b . . . 
+    . . . . b d 5 5 5 4 b 4 4 b . . 
+    . . . . b 3 2 3 5 5 4 e 4 4 b . 
+    . . . b d 2 2 2 5 7 5 4 e 4 4 e 
+    . . . b 5 3 2 3 5 5 5 5 e e e e 
+    . . b d 7 5 5 5 3 2 3 5 5 e e e 
+    . . b 5 5 5 5 5 2 2 2 5 5 d e e 
+    . b 3 2 3 5 7 5 3 2 3 5 d d e 4 
+    . b 2 2 2 5 5 5 5 5 5 d d e 4 . 
+    b d 3 2 d 5 5 5 d d d 4 4 . . . 
+    b 5 5 5 5 d d 4 4 4 4 . . . . . 
+    4 d d d 4 4 4 . . . . . . . . . 
+    4 4 4 4 . . . . . . . . . . . . 
+    `, SpriteKind.Player)
 music.baDing.play()
-tiles.setCurrentTilemap(tilemap`level1`)
-scene.cameraFollowSprite(mySprite)
